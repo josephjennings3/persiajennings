409b147682ca76516c05b37298bc787d09759fdd
Joseph Jennings <josephjennings@Josephs-MacBook-Pro.local>
2019-12-17 20:34:44 -0800

    updated header. active styling and colors

--- src/components/header.js
+++ src/components/header.js
@@ -14,26 +14,26 @@ class Header extends Component {
             <div className="header_list">
                 <ul>
                     <li>
-                        <NavLink to ='/pages/home' exact 
+                        <NavLink className="link_style" to ='/pages/home' exact 
                         activeStyle={
-                                {borderTop: 'solid 3px #000'}}>Home</NavLink>
+                                {borderTop: 'solid 3px #cb2d6f'}}>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to ='/pages/stats' exact 
+                        <NavLink className="link_style" to ='/pages/stats' exact 
                             activeStyle={
-                                    {borderTop: 'solid 3px #000'}}>
+                                    {borderTop: 'solid 3px #cb2d6f'}}>
                             Stats</NavLink>
                     </li>
                     <li>
-                        <NavLink to ='/pages/gallery' exact 
+                        <NavLink className="link_style" to ='/pages/gallery' exact 
                             activeStyle={
-                                    {borderTop: 'solid 3px #000'}}>
+                                    {borderTop: 'solid 3px #cb2d6f'}}>
                             Gallery</NavLink>
                     </li>
                     <li>
-                        <NavLink to ='/pages/dedication' exact 
+                        <NavLink className="link_style" to ='/pages/dedication' exact 
                             activeStyle={
-                                {borderTop: 'solid 3px #000'}}>
+                                {borderTop: 'solid 3px #cb2d6f'}}>
                         Dedication</NavLink>
                     </li>
                 </ul>

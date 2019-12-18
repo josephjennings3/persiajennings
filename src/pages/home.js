45c6010b0adfa4607989ffe8191c383935cba1ec
Joseph Jennings <josephjennings@Josephs-MacBook-Pro.local>
2019-12-17 23:27:11 -0800

    added icons and lorem ipsum to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,11 @@ import overalls from '../imgs/overalls.jpg';
 import look from '../imgs/look.jpg';
 import flowerdress from '../imgs/flowerdress.jpg';
 import flowergirl from '../imgs/flowergirl.jpg';
+import supergirl from '../imgs/super.png';
+import love from '../imgs/love.png';
+import smart from '../imgs/smart.png';
+
+
 
 
 class Home extends Component {
@@ -40,8 +45,28 @@ class Home extends Component {
                         </div>
                     </Carousel>
                 </div>
-                <div className="home_text">
-                    <h1>Greatest Child Ever Created</h1>
+                <div className="home_container">
+                    <div className="home_header">
+                        <h1>Greatest Child Ever Created</h1>
+                    </div>
+                    <div className="home_text">
+                        <p>  <t/>  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales sapien sit amet auctor ultrices. Suspendisse molestie nisi dolor, quis facilisis magna pharetra sit amet. Morbi mattis lacus ac ipsum gravida, nec ultrices neque hendrerit. Pellentesque eget diam eu dui molestie vestibulum. Quisque viverra magna id dolor sagittis hendrerit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris gravida gravida dolor eu pulvinar.</p> <br/>
+                        <p>Nam condimentum ligula eu urna aliquet, a euismod metus imperdiet. Duis ornare dictum augue vestibulum luctus. Cras faucibus magna nulla, nec cursus lorem consequat eget. Nunc ac posuere quam. Proin et sem a dui scelerisque semper ut eu sem. Quisque lorem felis, pretium ut est congue, egestas mollis augue. Nam dictum magna non elit gravida, aliquam interdum leo ultrices.</p>
+                    </div>
+                    <div className="banner">
+                        <div className="icon_box">
+                            <img src={supergirl} />
+                            Radiating Positivity
+                        </div>
+                        <div className="icon_box">
+                            <img src={love} />
+                            Always loving
+                        </div>
+                        <div className="icon_box">
+                            <img src={smart} />
+                            Very Smart
+                        </div>
+                    </div>
                 </div>
             </div>
         )
